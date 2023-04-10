c86df9ff72bf121ca70f2232d5e6c1ec1a2b9364
Georgi Atanasov <gatanasov@vmware.com>
2023-04-10 13:50:49 +0300

    Lab 6 - route lib
    nx generate @nrwl/angular:component game-detail --project=store-feature-game-detail

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -4,13 +4,20 @@ import { MatCardModule } from "@angular/material/card";
 import { CommonModule } from "@angular/common";
 import { StoreUiSharedModule } from "@bg-hoard/store/ui-shared";
 import { formatRating } from "@bg-hoard/store/util-formatters";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./app.routes";
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatCardModule, CommonModule, StoreUiSharedModule]
+  imports: [
+    MatCardModule,
+    CommonModule,
+    StoreUiSharedModule,
+    RouterModule,
+  ]
 })
 export class AppComponent {
   title = 'Board Game Hoard';
